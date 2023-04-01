2d6ca573781fd9ecc75f8bd6b2732d9d76aa3f18
Francisco J. Luna <fjluna@outlook.com>
2023-04-01 15:32:55 +0200

    controlado valores de campos en la busqueda avanzada

--- resources/views/consulta/index.blade.php
+++ resources/views/consulta/index.blade.php
@@ -14,13 +14,20 @@
                 <div class="form-group row mb-2">
 
                     <div class="col-md-2">
-                        <input type="text" id="cod_interno" name="cod_interno" placeholder="Cod. Interno" class="form-control">
+                        <div class="input-group">
+                            <input type="text" id="cod_interno" name="cod_interno" placeholder="Cod. Interno" class="form-control" value="{{Request::capture()->get('cod_interno')!=null ? Request::capture()->get('cod_interno') : ''}}">
+                            <button class="btn btn-outline-secondary" type="button" id="reset_cod_interno"><i class="fa fa-times" aria-hidden="true"></i></button>
+                        </div>
                     </div>
+
                     <div class="col-md-4">
                         <select class="form-select" name="tecnico" id="tecnico">
                             <option value="">-- Técnico --</option>
                             @foreach ($tecnicos as $tecnico)
-                            <option value="{{$tecnico->id}}">{{$tecnico->username}}</option>
+                            <option value="{{$tecnico->id}}" {{old('tecnico') == $tecnico->id ? 'selected' : (Request::capture()->get('tecnico') == $tecnico->id ? 'selected' : '')}}>
+                                {{$tecnico->username}}
+                            </option>
+
                             @endforeach
                         </select>
                     </div>
@@ -28,7 +35,9 @@
                         <select class="form-select" name="persona" id="persona">
                             <option value="">-- Persona --</option>
                             @foreach ($personas as $persona)
-                            <option value="{{$persona->id}}">{{$persona->apellidos.", ".$persona->nombre}}</option>
+                            <option value="{{$persona->id}}" {{old('persona') == $persona->id ? 'selected' : (Request::capture()->get('persona') == $persona->id ? 'selected' : '')}}>
+                                {{$persona->apellidos.", ".$persona->nombre}}
+                            </option>
                             @endforeach
                         </select>
                     </div>
@@ -37,24 +46,24 @@
 
                     <div class="col-md-2">
                         <div class="input-group">
-                            <input type="text" id="f_oper_ini" name="f_oper_ini" placeholder="Desde fecha operación..." class="form-control campo_fecha">
+                            <input type="text" id="f_oper_ini" name="f_oper_ini" placeholder="Desde fecha operación..." class="form-control campo_fecha" value="{{Request::capture()->get('f_oper_ini')!=null ? Request::capture()->get('f_oper_ini') : ''}}">
                             <button class="btn btn-outline-secondary" type="button" id="reset_f_oper_ini"><i class="fa fa-times" aria-hidden="true"></i></button>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <select class="form-select" name="tipo_operacion" id="tipo_operacion">
                             <option value="">-- Tipo de Operación --</option>
-                            <option value="instalacion">instalación</option>
-                            <option value="reparacion">reparación</option>
-                            <option value="almacenaje">almacenaje</option>
-                            <option value="baja">baja</option>
+                            <option value="instalacion" {{old('tipo_operacion') == 'instalacion' ? 'selected' : (Request::capture()->get('tipo_operacion') == 'instalacion' ? 'selected' : '')}}>instalación</option>
+                            <option value="reparacion" {{old('tipo_operacion') == 'reparacion' ? 'selected' : (Request::capture()->get('tipo_operacion') == 'reparacion' ? 'selected' : '')}}>reparación</option>
+                            <option value="almacenaje" {{old('tipo_operacion') == 'almacenaje' ? 'selected' : (Request::capture()->get('tipo_operacion') == 'almacenaje' ? 'selected' : '')}}>almacenaje</option>
+                            <option value="baja" {{old('tipo_operacion') == 'baja' ? 'selected' : (Request::capture()->get('tipo_operacion') == 'baja' ? 'selected' : '')}}>baja</option>
                         </select>
                     </div>
                     <div class="col-md-6">
                         <select class="form-select" name="ubicacion" id="ubicacion">
                             <option value="">-- Ubicación --</option>
                             @foreach ($ubicaciones as $ubicacion)
-                            <option value="{{$ubicacion->id}}">{{$ubicacion->servicio." - ".$ubicacion->dependencia}}</option>
+                            <option value="{{$ubicacion->id}}" {{old('ubicacion') == $ubicacion->id ? 'selected' : (Request::capture()->get('ubicacion') == $ubicacion->id ? 'selected' : '')}}>{{$ubicacion->servicio." - ".$ubicacion->dependencia}}</option>
                             @endforeach
                         </select>
                     </div>
@@ -64,7 +73,7 @@
 
                     <div class="col-md-2">
                         <div class="input-group">
-                            <input type="text" id="f_oper_fin" name="f_oper_fin" placeholder="Hasta fecha operación..." class="form-control campo_fecha">
+                            <input type="text" id="f_oper_fin" name="f_oper_fin" placeholder="Hasta fecha operación..." class="form-control campo_fecha" value="{{Request::capture()->get('f_oper_fin')!=null ? Request::capture()->get('f_oper_fin') : ''}}">
                             <button class="btn btn-outline-secondary" type="button" id="reset_f_oper_fin"><i class="fa fa-times" aria-hidden="true"></i></button>
                         </div>
                     </div>
@@ -72,7 +81,7 @@
                         <select class="form-select" name="tipo_equipo" id="tipo_equipo">
                             <option value="">-- Tipo de Equipo --</option>
                             @foreach ($tipos as $tipo)
-                            <option value="{{$tipo->id}}">{{$tipo->tipo}}</option>
+                            <option value="{{$tipo->id}}" {{old('tipo_equipo') == $tipo->id ? 'selected' : (Request::capture()->get('tipo_equipo') == $tipo->id ? 'selected' : '')}}>{{$tipo->tipo}}</option>
                             @endforeach
                         </select>
                     </div>
@@ -80,7 +89,7 @@
                         <select class="form-select" name="contratacion" id="contratacion">
                             <option value="">-- Contratación --</option>
                             @foreach ($contrataciones as $contratacion)
-                            <option value="{{$contratacion->id}}">{{$contratacion->empresa . ": Desde ". $contratacion->fecha_inicio . " hasta " . $contratacion->fecha_fin}}</option>
+                            <option value="{{$contratacion->id}}" {{old('contratacion') == $contratacion->id ? 'selected' : (Request::capture()->get('contratacion') == $contratacion->id ? 'selected' : '')}}>{{$contratacion->empresa . ": Desde ". $contratacion->fecha_inicio . " hasta " . $contratacion->fecha_fin}}</option>
                             @endforeach
                         </select>
                     </div>
@@ -92,7 +101,7 @@
                         <select class="form-select" name="marca" id="marca">
                             <option value="">-- Marca --</option>
                             @foreach ($marcas as $marca)
-                            <option value="{{$marca}}">{{$marca}}</option>
+                            <option value="{{$marca}}" {{old('marca') == $marca ? 'selected' : (Request::capture()->get('marca') == $marca ? 'selected' : '')}}>{{$marca}}</option>
                             @endforeach
                         </select>
                     </div>
@@ -100,16 +109,23 @@
                         <select class="form-select" name="modelo" id="modelo">
                             <option value="">-- Modelo --</option>
                             @foreach ($modelos as $modelo)
-                            <option value="{{$modelo}}">{{$modelo}}</option>
+                            <option value="{{$modelo}}" {{old('modelo') == $modelo ? 'selected' : (Request::capture()->get('modelo') == $modelo ? 'selected' : '')}}>{{$modelo}}</option>
                             @endforeach
                         </select>
                     </div>
                     <div class="col-md-3">
-                        <input type="text" id="product_number" name="product_number" placeholder="Product Number" class="form-control">
+                        <div class="input-group">
+                            <input type="text" id="product_number" name="product_number" placeholder="Product Number" class="form-control" value="{{Request::capture()->get('product_number')!=null ? Request::capture()->get('product_number') : ''}}">
+                            <button class="btn btn-outline-secondary" type="button" id="reset_product_number"><i class="fa fa-times" aria-hidden="true"></i></button>
+                        </div>
                     </div>
                     <div class="col-md-3">
-                        <input type="text" id="num_serie" name="num_serie" placeholder="Número de Serie" class="form-control">
+                        <div class="input-group">
+                            <input type="text" id="num_serie" name="num_serie" placeholder="Número de Serie" class="form-control" value="{{Request::capture()->get('num_serie')!=null ? Request::capture()->get('num_serie') : ''}}">
+                            <button class="btn btn-outline-secondary" type="button" id="reset_num_serie"><i class="fa fa-times" aria-hidden="true"></i></button>
+                        </div>
                     </div>
+
                 </div>
 
                 <div class="form-group row mb-2">
@@ -117,14 +133,16 @@
 
                     <div class="col-md-11">
                         <div class="form-check">
-                            <input class="form-check-input" type="checkbox" name="activa" id="activa">
+                            <input class="form-check-input" type="checkbox" name="activa" id="activa" {{old('activa') == 'on' ? 'checked' : (Request::capture()->get('activa') == 'on' ? 'checked' : '')}}>
                             <label class="form-check-label" for="activa">
                                 Mostrar sólo operación activa
                             </label>
                         </div>
                     </div>
-                    <div class="col-md-1">
+                    <div class="col-md-1 d-flex justify-content-end">
                         <input type="submit" class="btn btn-success" value="Filtrar">
+                        <a href="{{ route('consulta.index') }}" class="btn btn-primary ms-2">Reset</a>
+
                     </div>
                 </div>
             </form>
@@ -186,6 +204,18 @@
 
         $('#reset_f_oper_fin').click(function() {
             $('#f_oper_fin').val('');
+        });
+
+        $('#reset_cod_interno').click(function() {
+            $('#cod_interno').val('');
+        });
+
+        $('#reset_num_serie').click(function() {
+            $('#num_serie').val('');
+        });
+
+        $('#reset_product_number').click(function() {
+            $('#product_number').val('');
         });
 
     });
